0883dd5d78b652751ebe15d84667b55ef2e87b47
Juan Rodas <juan.rodas@pingponginnovation.com>
2019-10-15 21:28:33 -0500

    modify getDocuments

--- CognitiveSearch.UI/Controllers/HomeController.cs
+++ CognitiveSearch.UI/Controllers/HomeController.cs
@@ -53,7 +53,21 @@ namespace CognitiveSearch.UI.Controllers
         [HttpPost]
         public IActionResult GetDocuments(string q = "", SearchFacet[] searchFacets = null, int currentPage = 1)
         {
-            var token = GetContainerSasUri();
+            string sasContainerToken;
+            string accountName = _configuration.GetSection("StorageAccountName")?.Value;
+            string accountKey = _configuration.GetSection("StorageAccountKey")?.Value;
+            string containerAddress = _configuration.GetSection("StorageContainerAddress")?.Value;
+            CloudBlobContainer container = new CloudBlobContainer(new Uri(containerAddress), new StorageCredentials(accountName, accountKey));
+
+            SharedAccessBlobPolicy adHocPolicy = new SharedAccessBlobPolicy()
+            {
+                SharedAccessExpiryTime = DateTime.UtcNow.AddHours(1),
+                Permissions = SharedAccessBlobPermissions.Read
+            };
+
+            sasContainerToken = container.GetSharedAccessSignature(adHocPolicy, null);
+            
+            var token = sasContainerToken;
             var selectFilter = _docSearch.Model.SelectFilter;
 
             if (!string.IsNullOrEmpty(q))
@@ -105,20 +119,7 @@ namespace CognitiveSearch.UI.Controllers
 
         private string GetContainerSasUri()
         {
-            string sasContainerToken;
-            string accountName = _configuration.GetSection("StorageAccountName")?.Value;
-            string accountKey = _configuration.GetSection("StorageAccountKey")?.Value;
-            string containerAddress = _configuration.GetSection("StorageContainerAddress")?.Value;
-            CloudBlobContainer container = new CloudBlobContainer(new Uri(containerAddress), new StorageCredentials(accountName, accountKey));
-
-            SharedAccessBlobPolicy adHocPolicy = new SharedAccessBlobPolicy()
-            {
-                SharedAccessExpiryTime = DateTime.UtcNow.AddHours(1),
-                Permissions = SharedAccessBlobPermissions.Read
-            };
-
-            sasContainerToken = container.GetSharedAccessSignature(adHocPolicy, null);
-            return sasContainerToken;
+            
         }
     }
 }
